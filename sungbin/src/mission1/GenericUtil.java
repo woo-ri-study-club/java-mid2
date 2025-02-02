4df073fcf180d3d0842f02904053a6d3fd7bfed4
SungbinYang <ysb05222@naver.com>
2025-02-03 05:15:14 +0900

    feat. 섹션3 미션 제출

---
+++ sungbin/src/mission1/GenericUtil.java
@@ -0,0 +1,65 @@
+package mission1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericUtil {
+
+    public static <T extends Comparable<? super T>> T max(List<? extends T> list) {
+        if (list.isEmpty()) {
+            throw new IllegalArgumentException("리스트가 비어 있습니다.");
+        }
+
+        T max = list.get(0);
+
+        for (T element : list) {
+            if (element.compareTo(max) > 0) {
+                max = element;
+            }
+        }
+
+        return max;
+    }
+
+    public static <T extends Comparable<? super T>> List<T> mergeSortedLists(List<? extends T> list1, List<? extends T> list2) {
+        List<T> mergedList = new ArrayList<>();
+        int i = 0, j = 0;
+        int size1 = list1.size(), size2 = list2.size();
+
+        while (i < size1 && j < size2) {
+            T elem1 = list1.get(i);
+            T elem2 = list2.get(j);
+            if (elem1.compareTo(elem2) <= 0) {
+                mergedList.add(elem1);
+                i++;
+            } else {
+                mergedList.add(elem2);
+                j++;
+            }
+        }
+
+        while (i < size1) {
+            mergedList.add(list1.get(i++));
+        }
+
+        while (j < size2) {
+            mergedList.add(list2.get(j++));
+        }
+
+        return mergedList;
+    }
+
+    public static <T extends Number> double average(T[] array) {
+        if (array.length == 0) {
+            throw new IllegalArgumentException("배열이 비어 있습니다.");
+        }
+
+        double sum = 0.0;
+
+        for (T number : array) {
+            sum += number.doubleValue();
+        }
+
+        return sum / array.length;
+    }
+}
