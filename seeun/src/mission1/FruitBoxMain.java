f35c72f61a8be4584e404d2c02c25e3a9fc0849f
tdronnie <senyvee9@gmail.com>
2025-02-09 19:43:57 +0900

    refactor. Fruit 구현체 변수 추가 (혜민님)

--- seeun/src/mission1/FruitBoxMain.java
+++ seeun/src/mission1/FruitBoxMain.java
@@ -8,13 +8,13 @@ public class FruitBoxMain {
     public static void main(String[] args) {
 
         FruitBox<Apple> fruitBox1 = new FruitBox<>();
-        fruitBox1.store(new Apple("맛있는 사과", 10000, "red", 5));
-        fruitBox1.store(new Apple("덜 맛있는 사과", 5000, "purple", 3));
+        fruitBox1.store(new Apple("맛있는 사과", 10000, "red", 5, 5));
+        fruitBox1.store(new Apple("덜 맛있는 사과", 5000, "purple", 3, 10));
 
         FruitBox<Banana> fruitBox2 = new FruitBox<>();
-        fruitBox2.store(new Banana("썩은 바나나", 15000, "black", 2));
-        fruitBox2.store(new Banana("맛있는 바나나", 8000, "yellow", 5));
-        fruitBox2.store(new Banana("덜 익은 바나나", 10000, "green", 1));
+        fruitBox2.store(new Banana("썩은 바나나", 15000, "black", 2, 7));
+        fruitBox2.store(new Banana("맛있는 바나나", 8000, "yellow", 5, 10));
+        fruitBox2.store(new Banana("덜 익은 바나나", 10000, "green", 1, 15));
 
         FruitBoxInfo.checkRipeness(fruitBox1);
         FruitBoxInfo.checkRipeness(fruitBox2);
--- seeun/src/mission1/fruits/Banana.java
+++ seeun/src/mission1/fruits/Banana.java
@@ -2,14 +2,20 @@ package mission1.fruits;
 
 public class Banana extends Fruit{
 
-    public Banana(String name, int price, String color, int sweetness) {
+    private int length;
+
+    public Banana(String name, int price, String color, int sweetness, int length) {
         super(name, price, color, sweetness);
+        this.length = length;
+    }
+
+    public int getLength() {
+        return length;
     }
 
     @Override
     public void showInfo() {
-        System.out.println(getName() + ": 가격: " + getPrice() + ", 당도: " + getSweetness());
-
+        System.out.println(getName() + ": 가격: " + getPrice() + ", 색: " + getColor() + ", 당도: " + getSweetness() + ", 길이: " + getLength());
     }
 
     @Override
