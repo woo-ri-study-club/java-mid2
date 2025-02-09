ca9f80858bc261fe3bc77956ee61c225bd05cc59
hmkim199 <hmkim199@gmail.com>
2025-02-09 19:43:52 +0900

    refactor. 리뷰 반영 by 조채원님

--- hyemin/mission1/GenericIntersection.java
+++ hyemin/mission1/GenericIntersection.java
@@ -5,15 +5,10 @@ import java.util.List;
 import java.util.Set;
 
 public class GenericIntersection {
-    public static <T extends Comparable<? super T>> List<T> findCommonElement(List<? extends T> list1, List<? extends T> list2) {
-        Set<T> commonElements = new HashSet<>();
+    public static <T extends Comparable<? super T>> Set<T> findCommonElement(List<? extends T> list1, List<? extends T> list2) {
+        Set<T> set1 = new HashSet<>(list1);
+        set1.retainAll(list2);
 
-        for (T t : new HashSet<>(list1)) {
-            if (list2.contains(t)) {
-                commonElements.add(t);
-            }
-        }
-
-        return commonElements.stream().toList();
+        return set1;
     }
 }
--- hyemin/mission1/GenericMain.java
+++ hyemin/mission1/GenericMain.java
@@ -1,6 +1,7 @@
 package mission1;
 
 import java.util.List;
+import java.util.Set;
 
 import static mission1.GenericIntersection.findCommonElement;
 
@@ -8,12 +9,12 @@ public class GenericMain {
     public static void main(String[] args) {
         List<Integer> integerList1 = List.of(1, 2, 3, 4);
         List<Integer> integerList2 = List.of(2, 4);
-        List<Integer> commonIntegers = findCommonElement(integerList1, integerList2);
+        Set<Integer> commonIntegers = findCommonElement(integerList1, integerList2);
         System.out.println("commonIntegers = " + commonIntegers); // commonIntegers = [2, 4]
 
         List<String> stringList1 = List.of("1", "2", "3");
         List<String> stringList2 = List.of("1", "2", "3", "5");
-        List<String> commonStrings = findCommonElement(stringList1, stringList1);
+        Set<String> commonStrings = findCommonElement(stringList1, stringList1);
         System.out.println("commonStrings = " + commonStrings); // commonStrings = [1, 2, 3]
     }
 }
