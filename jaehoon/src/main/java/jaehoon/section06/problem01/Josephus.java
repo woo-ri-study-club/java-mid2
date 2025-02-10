619d6045117e8e0d98d906a805aa36db807819af
ljh468 <ljh468@naver.com>
2025-02-11 01:57:05 +0900

    fix. 사람수(N)이 0보다 작은경우, 제거 간격이 음수거나 0인 경우 예외처리 추가  with 리뷰반영

--- jaehoon/src/main/java/jaehoon/section06/problem01/Josephus.java
+++ jaehoon/src/main/java/jaehoon/section06/problem01/Josephus.java
@@ -7,20 +7,50 @@ public class Josephus {
 
   private final List<Integer> people;
 
-  public Josephus(List<Integer> people) {
+  public Josephus(int peopleCount, List<Integer> people) {
+    validatePeopleCount(peopleCount);
     this.people = people;
+    for (int i = 1; i <= peopleCount; i++) {
+      people.add(i);
+    }
+  }
+
+  private void validatePeopleCount(int peopleCount) {
+    if (peopleCount <= 0) {
+      throw new IllegalArgumentException("사람이 존재하지 않습니다.");
+    }
+  }
+
+  private void validateRemovalInterval(int interval) {
+    if (interval < 0) {
+      throw new IllegalArgumentException("제거 간격은 음수가 들어올 수 없습니다.");
+    }
+  }
+
+  public long getPeopleSize() {
+    return people.size();
   }
 
   public int lastRemainPerson(int K) {
+    validateRemovalInterval(K);
+    if (K == 0) {
+      return people.getLast();
+    }
+
     int currentIndex = 0;
     while (people.size() > 1) {
       currentIndex = (currentIndex + K - 1) % people.size();
-          people.remove(currentIndex);
+      people.remove(currentIndex);
     }
-    return people.get(0);
+    return people.getFirst();
   }
 
   public int lastRemainPersonIter(int K) {
+    validateRemovalInterval(K);
+    if (K == 0) {
+      return people.getLast();
+    }
+
     ListIterator<Integer> iterator = people.listIterator();
     while (people.size() > 1) {
       for (int i = 0; i < K; i++) {
@@ -32,6 +62,7 @@ public class Josephus {
       }
       iterator.remove();
     }
-    return people.get(0);
+
+    return people.getFirst();
   }
 }
--- jaehoon/src/main/java/jaehoon/section06/problem01/JosephusMain.java
+++ jaehoon/src/main/java/jaehoon/section06/problem01/JosephusMain.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.List;
 
 public class JosephusMain {
 
@@ -14,34 +13,30 @@ public class JosephusMain {
     int N = Integer.parseInt(br.readLine());
     int K = Integer.parseInt(br.readLine());
 
-    List<Integer> arrayList = new ArrayList<>();
-    for (int i = 1; i <= N; i++) {
-      arrayList.add(i);
+    try {
+      Josephus josephusArrayList = new Josephus(N, new ArrayList<>());
+      long startArray = System.currentTimeMillis();
+      System.out.println("최종 생존자: " + josephusArrayList.lastRemainPerson(K));
+      long endArray = System.currentTimeMillis();
+      System.out.println("ArrayList 실행 시간: " + (endArray - startArray) + "ms");
+      System.out.println();
+
+      Josephus josephusLinkedList = new Josephus(N, new LinkedList<>());
+      long startLinked = System.currentTimeMillis();
+      System.out.println("최종 생존자: " + josephusLinkedList.lastRemainPerson(K));
+      long endLinked = System.currentTimeMillis();
+      System.out.println("LinkedList 실행 시간: " + (endLinked - startLinked) + "ms");
+      System.out.println();
+
+      Josephus josephusLinkedList2 = new Josephus(N, new LinkedList<>());
+      long startLinkedIter = System.currentTimeMillis();
+      System.out.println("최종 생존자: " + josephusLinkedList2.lastRemainPersonIter(K));
+      long endLinkedIter = System.currentTimeMillis();
+      System.out.println("LinkedList + Iterator 실행 시간: " + (endLinkedIter - startLinkedIter) + "ms");
+    } catch (Exception exception) {
+      exception.printStackTrace();
+      System.out.println("exception.getMessage() = " + exception.getMessage());
     }
-
-    List<Integer> linkedList = new LinkedList<>();
-    for (int i = 1; i <= N; i++) {
-      linkedList.add(i);
-    }
-
-    Josephus josephusArrayList = new Josephus(arrayList);
-    long startArray = System.currentTimeMillis();
-    System.out.println("최종 생존자: " + josephusArrayList.lastRemainPerson(K));
-    long endArray = System.currentTimeMillis();
-    System.out.println("ArrayList 실행 시간: " + (endArray - startArray) + "ms");
-    System.out.println();
-
-    Josephus josephusLinkedList = new Josephus(linkedList);
-    long startLinked = System.currentTimeMillis();
-    System.out.println("최종 생존자: " + josephusLinkedList.lastRemainPerson(K));
-    long endLinked = System.currentTimeMillis();
-    System.out.println("LinkedList 실행 시간: " + (endLinked - startLinked) + "ms");
-    System.out.println();
-
-    long startLinkedIter = System.currentTimeMillis();
-    System.out.println("최종 생존자: " + josephusLinkedList.lastRemainPersonIter(K));
-    long endLinkedIter = System.currentTimeMillis();
-    System.out.println("LinkedList + Iterator 실행 시간: " + (endLinkedIter - startLinkedIter) + "ms");
     br.close();
   }
 }
