777ad7924c6a6c72fba51d2d0d95104289969332
tdronnie <senyvee9@gmail.com>
2025-02-11 00:57:00 +0900

    feat. 섹션9 미션제출

---
+++ seeun/src/mission3/UniqueStrings.java
@@ -0,0 +1,24 @@
+package mission3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class UniqueStrings {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Set<String> uniqueStrings = new TreeSet<>();
+
+        for (int i = 0; i < n; i++) {
+            uniqueStrings.add(br.readLine());
+        }
+
+        for (String string : uniqueStrings) {
+            System.out.println(string);
+        }
+    }
+}
