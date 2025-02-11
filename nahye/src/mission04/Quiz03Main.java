72202a8896b32eb0e35c0666e4079521c1e5bf6b
nahye <nnhh.shine@gmail.com>
2025-02-12 08:03:16 +0900

    섹션10 미션 문제3 완료

---
+++ nahye/src/mission04/Quiz03Main.java
@@ -0,0 +1,44 @@
+package mission04;
+
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+
+
+public class Quiz03Main {
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 3, -1, -3, 5, 3, 6, 7};
+        int k = 3;
+
+        int[] result = getMaxSlidingWindow(nums, k);
+        System.out.println(Arrays.toString(result)); // [3, 3, 5, 5, 6, 7]
+
+    }
+
+    private static int[] getMaxSlidingWindow(int[] nums, int k) {
+
+        Deque<Integer> deque = new LinkedList<>();
+        int[] result = new int[nums.length - k + 1];
+
+        for (int i = 0; i < nums.length; i++) {
+            if (!deque.isEmpty() && deque.peekFirst() == i - k) {
+                deque.pollFirst();
+            }
+
+            while (!deque.isEmpty() && nums[deque.peekLast()] < nums[i]) {
+                deque.pollLast();
+            }
+
+            deque.offerLast(i);
+
+            if (i >= k - 1) {
+                result[i - k + 1] = nums[deque.peekFirst()];
+            }
+        }
+
+        return result;
+    }
+
+}
