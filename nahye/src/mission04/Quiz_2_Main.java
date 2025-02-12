2ccdbe38de806c188f5cabaf7ffbded96c47a08a
nahye <nnhh.shine@gmail.com>
2025-02-12 10:08:10 +0900

    섹션10 미션 문제2 완성

---
+++ nahye/src/mission04/Quiz_2_Main.java
@@ -0,0 +1,38 @@
+package mission04;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Quiz_2_Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        System.out.println(isValidBrackets(input) ? "YES" : "NO");
+    }
+
+    private static boolean isValidBrackets(String input) {
+        Stack<Character> stack = new Stack<>();
+
+        for(char c : input.toCharArray()) {
+            if(c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            }else if (c == ')' || c == '}' || c == ']') {
+                if(stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.pop();
+
+                if ((c == ')' && top != '(')
+                        || (c == '}' && top != '{')
+                        || (c == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
