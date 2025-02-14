4d6c29b1685bf142b81e66f0aca256aa4aa82893
chaewonJo <ch730268@gmail.com>
2025-02-14 18:41:37 +0900

    괄호 체크 문제 리뷰 반영

--- chaewon/src/mission04/ParenthesesChecker.java
+++ chaewon/src/mission04/ParenthesesChecker.java
@@ -6,29 +6,33 @@ public class ParenthesesChecker {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        Map<String, String> parentheses = new HashMap<>();
-        parentheses.put("(", ")");
-        parentheses.put("{", "}");
-        parentheses.put("[", "]");
+        Map<Character, Character> parentheses = new HashMap<>();
+        parentheses.put('(', ')');
+        parentheses.put('{', '}');
+        parentheses.put('[', ']');
 
         System.out.println("괄호로만 이루어진 문자열을 입력하세요.: ");
         String str = sc.nextLine();
-        String[] toArray = str.split("");
+        char[] toArray = str.toCharArray();
 
-        Deque<String> checker = new ArrayDeque<>();
+        Deque<Character> checker = new ArrayDeque<>();
 
-        for (String one : toArray) {
+        for (char one : toArray) {
             if (parentheses.keySet().contains(one)) {
                 checker.push(one);
             } else {
-                String pop = checker.pop();
-                if (!parentheses.get(pop).equals(one)) {
+                if (checker.isEmpty() || !parentheses.get(checker.pop()).equals(one)) {
                     System.out.println("NO");
                     return;
                 }
             }
         }
-        System.out.println("YES");
+
+        if (checker.isEmpty()) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
 
         sc.close();
     }
